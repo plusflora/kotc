c732328595832f4c881ccd403dc98310fac41e81
plusflora <v.bouquet707@gmail.com>
2024-06-26 15:57:28 -0700

    more basics

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import "../App.css";
+
+function Header() {
+  return (
+    <header>
+      <img
+        className="kotcteeth"
+        src={require("../assets/King-of-the-Cove-2024-LOGO-SHARK-TEETH-BLACK-Transparent-5x5inches.png")}
+        alt="kotc-logo"
+      />
+    </header>
+  );
+}
+
+export default Header;
